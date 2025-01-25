820b0067d9dde18c07bfc9b1fbf7ea67652ef27b
Hervé Labas <herve@metabase.com>
2025-01-25 18:51:56 +0100

    Remove unused badge variants

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -9,14 +9,11 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default: 'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
-        disabled: 'border-transparent bg-gray-500 text-primary-foreground hover:bg-gray/80',
         light: 'bg-white text-primary',
-        accent: 'border-transparent bg-sky-300 text-accent-foreground hover:bg-sky/80',
         secondary:
           'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
         destructive:
-          'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-        outline: 'text-foreground',
+          'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80'
       },
     },
     defaultVariants: {
